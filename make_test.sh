ab42b8107ea05284bba9daccae469b5b5775908b
Alfredo Cádiz <acadiz@gmail.com>
2017-04-10 17:29:29 -0300

    updated make_test for the new format

--- make_test.sh
+++ make_test.sh
@@ -6,10 +6,10 @@ sed -i -e 's/{$nombre_alumno}/Nombre del alumno/g' template_test.tex
 sed -i -e 's/{$titulo_memoria}/Titulo de la memoria/g' template_test.tex
 sed -i -e 's/{$mes}/Mes/g' template_test.tex
 sed -i -e 's/{$anno}/Anno/g' template_test.tex
-sed -i -e 's/{$profesor_guia}/Nombre del profesor guia/g' template_test.tex
+sed -i -e 's/{{$profesor_guia}}/{Profesor guia 1, Profesor guiaaa 2}/g' template_test.tex
 sed -i -e 's/{$carrera_titulo}/Nombre de la carrera/g' template_test.tex
 sed -i -e 's/{$memoria}/MEMORIA PARA OPTAR AL TITULO DE/g' template_test.tex
-sed -i -e 's/{{$comision}}/{Profesor1}{Profesor2}{Profesor3}/g' template_test.tex
+sed -i -e 's/{{$comision}}/{Profesor1,Profesor2,Profesor3}/g' template_test.tex
 sed -i -e 's/{$resumen}/Este es el abstract/g' template_test.tex
 rm template_test.tex-e
 
